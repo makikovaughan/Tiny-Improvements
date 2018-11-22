24888e19cc7d3b257779c335e49b8a14390bf089
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-11-22 12:36:38 -0500

    Created a connection setup for heroku

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect('mongodb://localhost/kudo_db', { useNewUrlParser: true });
-
+//For Heroku
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  //Local URL
+  mongoose.connect('mongodb://localhost/kudo_db', { useNewUrlParser: true });
+}
 
 require('./routes/apiRoutes')(app);
 require('./routes/html-route')(app);
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`App running on port ${PORT}`);
 });
